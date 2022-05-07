8919b70ee656b110bd79fa02824bbb070f203a82
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2022-05-07 14:48:49 +0100

    postgre configured

--- first_deployment/settings_test.py
+++ first_deployment/settings_test.py
@@ -2,9 +2,18 @@
 
 ALLOWED_HOSTS = []
 
-# SECURITY WARNING: keep the secret key used in production secret!
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
+        'NAME': 'first',
+        'USER': 'first',
+        'PASSWORD': 'meatballs',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
-SECRET_KEY = 'django-insecure-9j!ya=7+jwgih%6xq9pvp=bbmquddxo*34)k_i@2!t!8*kqo3w'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
